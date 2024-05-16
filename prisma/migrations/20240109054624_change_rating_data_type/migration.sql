10d2e2c33b70f434dc0971c6de124b0e9323a0b4
dimsum <101172637+yaboidimsum@users.noreply.github.com>
2024-05-16 22:06:46 +0700

    update prisma for supa

--- prisma/migrations/20240109054624_change_rating_data_type/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to alter the column `rating` on the `comment` table. The data in that column could be lost. The data in that column will be cast from `Int` to `Decimal(3,2)`.
-
-*/
--- AlterTable
-ALTER TABLE `comment` MODIFY `rating` DECIMAL(3, 2) NOT NULL;
